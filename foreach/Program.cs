a0344283e765080d79591dd3d4642b8c0cddc0c0
serhanayberkkilic <ayberk.kilic@peakup.org>
2023-03-26 14:43:23 +0300

    for-each

---
+++ foreach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+
+
+namespace foreachNamespace
+{
+    
+    class Program
+    {
+        /// While loop
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sayac = 0;
+            int toplam = 0;
+
+            while (sayac<=number)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine("Toplam: {0}, Ortalama: {1}", toplam, toplam / number);
+            Program.test2(args);
+            
+
+        }
+
+        //a dan z ye kadar yazdırma
+        static void test2(string[] args)
+        {
+            char ch = 'a';
+            while (ch<='z')
+            {
+                Console.Write(ch + " ");
+                ch++;
+            }
+        }
+
+        
+
+    }
+}
