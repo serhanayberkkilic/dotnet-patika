73ad11c03eecdd74f310cb49911b030f79b9c5d6
serhanayberkkilic <ayberk.kilic@peakup.org>
2023-03-26 13:44:28 +0300

    addedforloop

---
+++ for-loop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace for_loop
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+            Console.WriteLine("i = {0}", i);
+        }*/
+
+        /*static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine("i = {0}", i);
+            }
+        }*/
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine("i = {0}", i);
+            }
+        }
+    }
+}
