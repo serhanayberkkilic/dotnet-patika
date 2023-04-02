44c649172cec0f15d94fbbec6ca43d9711b101b6
serhanayberkkilic <ayberk.kilic@peakup.org>
2023-04-02 15:29:22 +0300

    add methods

--- array-class/Program.cs
+++ array-class/Program.cs
@@ -10,7 +10,7 @@ namespace arrayclass
     {
         static void Main(string[] args)
         {
-
+            /*
             int [] numbers = {2,23,3,42,17};
             // Not Sorted Array
 
@@ -42,6 +42,16 @@ namespace arrayclass
             Console.WriteLine("*********indexoff******");
             
             Console.WriteLine("42:"+Array.IndexOf(numbers,42));
+            */
+
+
+            int[]  newlist = {2,16,21,3,4,21,5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,19,20,16};
+
+            Console.WriteLine("*********indexoff******");
+            
+            int maxValue = newlist.Max(t => t);
+            int value = Array.FindAll(newlist, t => t == maxValue).Length;
+            Console.WriteLine("Max Value: {0}  Max Value Index: {1}", maxValue,value);
 
         }
 
---
+++ methods/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5;
+            int b=6;
+
+            Console.WriteLine("Sum =" + (a+b));
+
+            Console.WriteLine("With Func Sum =" + Add(a,b));
+
+            Methods m = new Methods();
+            m.Add(a, b);
+            m.AddandUpgrade(ref a, ref b);
+            Console.WriteLine("Default Sum =" + (a+b));
+        }
+
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+
+    }
+
+    class Methods
+    {
+        
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("With class public func Sum =" + (a + b));
+        }
+
+        public void AddandUpgrade(ref int a,ref int b)
+
+        {
+            a += 1;
+            b += 1;
+            Console.WriteLine("With class public(Use Referance) func Sum =" + (a + b));
+
+        }
+
+    }
+}
